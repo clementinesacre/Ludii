505bd6eb7078f70dfe93dcd34c6f2d8602d10d7b
clementinesacre <sacre.clementine@gmail.com>
2025-05-28 11:05:23 +0200

    fix: null pointer exeption

--- Player/src/supplementary/experiments/speed/PlayoutsPerSec.java
+++ Player/src/supplementary/experiments/speed/PlayoutsPerSec.java
@@ -151,8 +151,11 @@ public final class PlayoutsPerSec
 
 		// map initial moves to initial board, to be able to start a new run of the game
 		List<Move> initialMovesList = context.trial().generateCompleteMovesList();
-		for (int i=0; i<initialMovesList.size(); i++)
-			initialMovesList.set(i, GrowingBoard.generateNewMove(initialMovesList.get(i), false, GrowingBoard.mappedNewToInitIndexes()));			
+		if (GrowingBoard.mappedNewToInitIndexes() != null)
+		{
+			for (int i=0; i<initialMovesList.size(); i++)
+				initialMovesList.set(i, GrowingBoard.generateNewMove(initialMovesList.get(i), false, GrowingBoard.mappedNewToInitIndexes()));	
+		}
 		
 		GrowingBoard.reset();
 	}
